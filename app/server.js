9a6ed369c283f078767ed03d4adca2059851b37c
janoti <janotijr@yahoo.com.br>
2022-01-28 21:50:43 -0300

    Date in main page

--- app/server.js
+++ app/server.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = Date.now();
+const date_ob = Date.now();
+// current date
+// adjust 0 before single digit date
+let date = ("0" + date_ob.getDate()).slice(-2);
+
+// current month
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+// current year
+let year = date_ob.getFullYear();
+
+// current hours
+let hours = date_ob.getHours();
+
+// current minutes
+let minutes = date_ob.getMinutes();
+
+// current seconds
+let seconds = date_ob.getSeconds(); 
+ 
 // create express app
 const app = express();
 // Setup server port
@@ -11,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // define a root route
 app.get("/", (req, res) => {
-  res.send("DEVOPS/SRE - JANOTI - WELCOME - " + date);
+  res.send("DEVOPS/SRE - JANOTI - WELCOME - " + year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
 });
 // Require employee routes
 const userRoutes = require("./src/routes/users.routes");
