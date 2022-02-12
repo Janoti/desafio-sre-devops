2067ca656c50567c532cf51ac162c605216e9cd8
janoti <janotijr@yahoo.com.br>
2022-02-12 13:50:32 -0300

    .env dockercompose and nodeapp

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,11 +1,13 @@
 'use strict';
+require('dotenv').config();
+
 const mysql = require('mysql2');
 //local mysql db connection
 const dbConn = mysql.createConnection({
-  host     : "mysql-server",
-  user     : "root",
-  password : "root",
-  database : "desafio"
+  host     : "mysql-server", // mysql-server(KUBERNETES OU AWS) localhost (DOCKER)
+  user     : process.env.MYSQL_USER, //"root"
+  password : process.env.MYSQL_PASSWORD,//"root",
+  database : process.env.MYSQL_DATABASE//"desafio"
 });
 dbConn.connect(function(err) {
   if (err) throw err;
--- app/server.js
+++ app/server.js
@@ -1,25 +1,8 @@
+
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const date_ob = new Date();
-// current date
-// adjust 0 before single digit date
-const date = ("0" + date_ob.getDate()).slice(-2);
 
-// current month
-const month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-
-// current year
-const year = date_ob.getFullYear();
-
-// current hours
-const hours = date_ob.getHours();
-
-// current minutes
-const minutes = date_ob.getMinutes();
-
-// current seconds
-const seconds = date_ob.getSeconds(); 
- 
 // create express app
 const app = express();
 // Setup server port
@@ -29,8 +12,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 // define a root route
+
+const version = 1
+
 app.get("/", (req, res) => {
-  res.send("DEVOPS/SRE - JANOTI - WELCOME ##### (Error verion) ##### - " + year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
+  res.send("DEVOPS/SRE - JANOTI - WELCOME 1 Version - " + version);
 });
 // Require employee routes
 const userRoutes = require("./src/routes/users.routes");
